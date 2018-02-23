eba6633434c8c24ec01f8d919bddf24d448afe5f
yurkevich-serj <yurkevichserj@gmail.com>
2018-02-23 15:14:10 +0300

    done it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+'use strict'
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   
+  if(str.length%2 != 0){
+  	return false;
+  }
+  else{
+  
+  for(let i = str.length-1; i >= 0; i--){
+     for (let j = 0; j < bracketsConfig.length; j++) {
+       if (str[i] == bracketsConfig[j][0]) {
+         str = str.replace(bracketsConfig[j].join(''), '');
+       }
+     }
+   }
+   if (str == '') {
+     return true;
+   }
+   else {
+     return false;
+  }
+  }
 }
